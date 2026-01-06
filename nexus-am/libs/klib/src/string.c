96bff95da73e86695fd6261317f09ac9e8aa3717
tracer-ics2019 <tracer@njuics.org>
2026-01-06 23:55:31 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  23:55:31 up 11:22,  1 user,  load average: 0.15, 0.24, 0.27 7e0de2167ba33b6e4a61b46ea33e7c65f69637f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -7,7 +7,10 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  assert(dst != NULL && src != NULL);
+  char* ret = dst;
+  while ((*dst++ = *src++));
+  return ret;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -15,11 +18,24 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  assert(dst != NULL && src != NULL);
+  char* ret = dst;
+  // Find the end of dst
+  while (*dst) {
+    dst++;
+  }
+  // Copy src to the end of dst
+  while ((*dst++ = *src++));
+  return ret;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  assert(s1 != NULL && s2 != NULL);
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
@@ -27,14 +43,40 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  assert(v != NULL);
+  unsigned char* p = v;
+  unsigned char val = c;
+  while (n--) {
+    *p++ = val;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  assert(out != NULL && in != NULL);
+  // Handle the case where out and in might be the same
+  if (out == in) {
+    return out;
+  }
+  unsigned char* p_out = out;
+  const unsigned char* p_in = in;
+  while (n--) {
+    *p_out++ = *p_in++;
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  assert(s1 != NULL && s2 != NULL);
+  const unsigned char* p1 = s1;
+  const unsigned char* p2 = s2;
+  while (n-- > 0) {
+    if (*p1 != *p2) {
+      return *p1 - *p2;
+    }
+    p1++;
+    p2++;
+  }
   return 0;
 }
 
