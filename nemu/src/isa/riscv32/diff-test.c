20ee742be662bed1ce8f7ea810073f075f45dd0c
tracer-ics2019 <tracer@njuics.org>
2026-01-07 16:22:41 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  16:22:41 up 58 min,  1 user,  load average: 0.30, 0.19, 0.18 37e028afb938279b6f81eb2f3be9214d416f2527

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,7 +2,22 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < 32; i++) {
+    if ((ref_r->gpr[i]._32) != cpu.gpr[i]._32) {
+      printf("Differential testing failed at PC = 0x%x\n", pc);
+      printf("Register '%s' mismatch: ref = 0x%x, dut = 0x%x\n",
+             reg_name(i, 32), ref_r->gpr[i]._32, cpu.gpr[i]._32);
+      return false;
+    }
+  }
+
+  if (ref_r->pc != cpu.pc) {
+    printf("Differential testing failed at PC = 0x%x\n", pc);
+    printf("PC mismatch: ref = 0x%x, dut = 0x%x\n", ref_r->pc, cpu.pc);
+    return false;
+  }
+
+  return true;
 }
 
 void isa_difftest_attach(void) {
