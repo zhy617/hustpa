8e859e2193ace5b06226b3239c63dc97771de960
tracer-ics2019 <tracer@njuics.org>
2026-01-07 17:32:04 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  17:32:04 up  2:07,  2 users,  load average: 0.51, 0.36, 0.36 f10e0855015910abef40e475ae302d115606c9e

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,24 +2,24 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  // for (int i = 0; i < 32; i++) {
-  //   if ((ref_r->gpr[i]._32) != cpu.gpr[i]._32) {
-  //     printf("Differential testing failed at PC = 0x%x\n", pc);
-  //     printf("Register '%s' mismatch: ref = 0x%x, dut = 0x%x\n",
-  //            reg_name(i, 32), ref_r->gpr[i]._32, cpu.gpr[i]._32);
-  //     return false;
-  //   }
-  // }
+  for (int i = 0; i < 32; i++) {
+    if ((ref_r->gpr[i]._32) != cpu.gpr[i]._32) {
+      printf("Differential testing failed at PC = 0x%x\n", pc);
+      printf("Register '%s' mismatch: ref = 0x%x, dut = 0x%x\n",
+             reg_name(i, 32), ref_r->gpr[i]._32, cpu.gpr[i]._32);
+      return false;
+    }
+  }
 
-  // if (ref_r->pc != cpu.pc) {
-  //   printf("Differential testing failed at PC = 0x%x\n", pc);
-  //   printf("PC mismatch: ref = 0x%x, dut = 0x%x\n", ref_r->pc, cpu.pc);
-  //   return false;
-  // }
+  if (ref_r->pc != cpu.pc) {
+    printf("Differential testing failed at PC = 0x%x\n", pc);
+    printf("PC mismatch: ref = 0x%x, dut = 0x%x\n", ref_r->pc, cpu.pc);
+    return false;
+  }
 
-  // return true;
+  return true;
   
-  return false;
+  // return false;
 }
 
 void isa_difftest_attach(void) {
