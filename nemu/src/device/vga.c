c56e24a2d1f2aa468f8ffa56fbc323a4827893f2
tracer-ics2019 <tracer@njuics.org>
2026-01-07 20:53:17 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  20:53:17 up  5:29,  2 users,  load average: 0.01, 0.19, 0.31 967db8326b9b55a611d6b6cb456f70e2c8cd3960

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -30,7 +30,10 @@ static inline void update_screen() {
 
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
-  TODO();
+  // TODO();
+  if (is_write && (offset == (SYNC_PORT - SCREEN_PORT) || offset == (SYNC_MMIO - SCREEN_MMIO))) {
+    update_screen();
+  }
 }
 
 void init_vga() {
