348971a429b7fa10537b7f8acb652d3472fc113a
tracer-ics2019 <tracer@njuics.org>
2026-01-07 19:01:33 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  19:01:33 up  3:37,  2 users,  load average: 0.35, 0.44, 0.31 9a5764c3a0bed29fc2b7a55c75ee4bffa194f26e

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -18,7 +18,8 @@ make_EHelper(addi) {
 
 make_EHelper(add) {
   rtl_add(&s0, &id_src->val, &id_src2->val);
-  // rtl_sr(id_dest->reg, &s0, 4);
+  
+  rtl_sr(id_dest->reg, &s0, 4);
 
   print_asm_template3(add);
 }
