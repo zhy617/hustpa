f3ea0c4b759b44a409a5ee237bc425dd10456af0
tracer-ics2019 <tracer@njuics.org>
2026-01-13 20:13:04 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  20:13:04 up  2:24,  3 users,  load average: 0.23, 0.31, 0.50 d9435e6dc11b344ac39fdbfd1521a41628f16359

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -5,6 +5,7 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
+  printf("irq: cause = %x\n", c->cause);
   if (user_handler) {
     _Event ev = {0};
     switch (c->cause) {
