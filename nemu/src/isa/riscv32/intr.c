1434fb4341d4da68eff4d7aa57fc5ce4b084c526
tracer-ics2019 <tracer@njuics.org>
2026-01-13 16:16:27 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  16:16:27 up  5:00,  3 users,  load average: 0.59, 0.57, 0.40 e134f5873bd54736706fb500dd9f47326a3657e2

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -1,9 +1,13 @@
 #include "rtl/rtl.h"
+#include "cpu/decode.h"
 
 void raise_intr(uint32_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  decinfo.isa.sepc = epc;
+  decinfo.isa.scause = NO;
+  rtl_j(decinfo.isa.stvec);
 }
 
 bool isa_query_intr(void) {
