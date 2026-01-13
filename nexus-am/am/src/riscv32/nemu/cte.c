fa3785f5446c4551fded718bfcaaf1a07c8e203b
tracer-ics2019 <tracer@njuics.org>
2026-01-14 00:13:28 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  00:13:28 up  6:24,  3 users,  load average: 0.20, 0.12, 0.16 690f4251f59242d0dbde9d789d26c023dab1de5

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -24,7 +24,7 @@ _Context* __am_irq_handle(_Context *c) {
       printf("c->GPR1 = %d\n", c->GPR1);
       switch (c->GPR1) {
         case -1: ev.event = _EVENT_YIELD; break;
-        case  1: ev.event = _EVENT_SYSCALL; break;
+        default: ev.event = _EVENT_SYSCALL; break;
       }
       printf("Yield event\n"); 
       break;
