3380a93b09858a953958cf861757b9807b252d4b
tracer-ics2019 <tracer@njuics.org>
2026-01-13 17:00:43 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  17:00:43 up  5:44,  3 users,  load average: 0.33, 0.40, 0.41 e06dbefc27bf52bee9c2f6e8e6346cef5e49d6b5

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -3,7 +3,7 @@
 
 make_EHelper(csrrs) {
   // 1. Read the old value from CSR and store it in a temporary RTL register s1.
-  rtl_li(&s1, csr_read(id_src->imm));
+  rtl_li(&s1, csr_read(id_src2->val));
 
   // 2. Write the old value (now in s1) to the destination register rd.
   rtl_mv(&id_dest->val, &s1);
@@ -13,21 +13,21 @@ make_EHelper(csrrs) {
   rtl_or(&s0, &s1, &id_src->val);
 
   // 4. Write the new value (in s0) back to the CSR.
-  csr_write(id_src->imm, s0);
+  csr_write(id_src2->val, s0);
 
   print_asm_template3(csrrs);
 }
 
 make_EHelper(csrrw) {
     // 1. Read the old value from CSR and store it in a temporary RTL register s1.
-    printf("imm: %d\n", id_src->imm);
-    rtl_li(&s1, csr_read(id_src->imm));
+    printf("imm: %d\n", id_src2->val);
+    rtl_li(&s1, csr_read(id_src2->val));
     
     // 2. Write the old value (now in s1) to the destination register rd.
     rtl_mv(&id_dest->val, &s1);
     
     // 3. Write the value from rs1 (id_src->val) directly to the CSR.
-    csr_write(id_src->imm, id_src->val);
+    csr_write(id_src2->val, id_src->val);
     
     print_asm_template3(csrrw);
 }
