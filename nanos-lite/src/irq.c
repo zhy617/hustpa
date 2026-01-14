c72ccaf303fa1c506aaf3bdb93397624b03da4c9
tracer-ics2019 <tracer@njuics.org>
2026-01-14 12:20:03 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  12:20:03 up  2:09,  3 users,  load average: 0.30, 0.18, 0.21 b012093eb75751cf745b70665a8f4b383c269c36

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -12,7 +12,7 @@ static _Context* do_event(_Event e, _Context* c) {
       break;
     case _EVENT_SYSCALL:
       printf("Handling syscall event\n");
-      printf("a1 = %d\n", c->GPR1);
+      printf("a1 = %d\n", c->GPR2);
       return do_syscall(c);
 
     default: panic("Unhandled event ID = %d", e.event);
