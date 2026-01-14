2fbbbf39db62424d016a87ef182ceff17c697d53
tracer-ics2019 <tracer@njuics.org>
2026-01-14 22:57:02 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  22:57:02 up 12:46,  3 users,  load average: 1.24, 0.72, 0.43 c8083e348ea28e561a4f3db8ca5954675a7fd26

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,7 +21,7 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   int key = read_key();
-  bool is_keydown = key & 0x8000;
+  bool is_keydown = key & 0x8000? true : false;
   key = key & 0x7fff;
   int n = 0;
   if (key == _KEY_NONE) {
