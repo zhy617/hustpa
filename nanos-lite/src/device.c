fa762a719cf5d5204099c1e084a036da1ea6a54f
tracer-ics2019 <tracer@njuics.org>
2026-01-14 17:57:01 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  17:57:01 up  7:46,  3 users,  load average: 0.78, 0.37, 0.37 da1ad2f2efe67c2ccba7d3ebce012206c3adcc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,9 +2,11 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  printf("serial_write: buf=%x, offset=%d, len=%d\n", buf, offset, len);
   for(size_t i = 0; i < len; i++) {
     _putc(*((char *)buf + i));
   }
+  printf("fuckyoufuckyou!!!\n");
   return len;
 }
 
