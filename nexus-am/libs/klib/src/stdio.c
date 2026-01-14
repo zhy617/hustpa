c9b2f63223b16928e571b81da9e7ca50246afc64
tracer-ics2019 <tracer@njuics.org>
2026-01-14 23:14:12 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  23:14:12 up 13:03,  3 users,  load average: 0.47, 0.69, 0.63 6497d7e1794beda6d1f7b94793487ec85ee03087

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -188,7 +188,7 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   // assert(0);
   va_list ap;
   va_start(ap, fmt);
-  printf("n = %d\n", n);
+  // printf("n = %d\n", n);
   int ret = vsnprintf(out, 4096, fmt, ap);
   // int ret = vsprintf(out, fmt, ap);
   va_end(ap);
