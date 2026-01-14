a22d88af7f02c0580954f742d72142a8221072b1
tracer-ics2019 <tracer@njuics.org>
2026-01-14 19:09:58 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  19:09:58 up  8:59,  3 users,  load average: 0.36, 0.40, 0.29 1e1e2f0fb5fc3c35a26ea62bbda950a4b283dddd

--- nanos-lite/src/do_syscall.c
+++ nanos-lite/src/do_syscall.c
@@ -25,15 +25,15 @@ size_t sys_write(int fd, const void *buf, size_t len) {
   // for (size_t i = 0; i < 10; i++) {
   //   printf("buf[%d] = %c (0x%x)\n", i, ((char *)buf)[i], ((char *)buf)[i]);
   // }
-  // if (fd == 1 || fd == 2) {
-  //   for (size_t i = 0; i < len; i++) {
-  //     _putc(((char *)buf)[i]);
-  //   }
-  //   return len; // Return number of bytes written
-  // } else {
-  //   return -1; // Unsupported fd
-  // }
-  return fs_write(fd, buf, len);
+  if (fd == 1 || fd == 2) {
+    for (size_t i = 0; i < len; i++) {
+      _putc(((char *)buf)[i]);
+    }
+    return len; // Return number of bytes written
+  } else {
+    return -1; // Unsupported fd
+  }
+  // return fs_write(fd, buf, len);
 }
 
 size_t sys_read(int fd, void *buf, size_t len) {
