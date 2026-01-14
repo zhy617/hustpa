878b7d8d50a9620f7cd8f700955a3bb5df302e28
tracer-ics2019 <tracer@njuics.org>
2026-01-14 13:22:25 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  13:22:25 up  3:11,  3 users,  load average: 0.05, 0.09, 0.12 a6459a2f3cab9593882fa692fd54c93a8d7f6c2

--- navy-apps/libs/libc/src/platform/crt0.c
+++ navy-apps/libs/libc/src/platform/crt0.c
@@ -11,9 +11,9 @@ void _start(int argc, char *argv[], char *envp[]) {
   environ = env;
   int ret = main(argc, argv, envp);
   // write(1, &ret, sizeof(ret)); // debug
-  write(1, "Exit code: ", 11); // debug
-  write(1, (char *)&ret, sizeof(ret)); // debug
-  write(1, "\n", 1); // debug
+  // write(1, "Exit code: ", 11); // debug
+  // write(1, (char *)&ret, sizeof(ret)); // debug
+  // write(1, "\n", 1); // debug
   // printf("Program exited with return value %d\n", ret);
   exit(ret);
   assert(0);
