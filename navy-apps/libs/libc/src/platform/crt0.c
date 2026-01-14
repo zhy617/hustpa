00c617e0652cec51a0e198de95d496a9c0f9adf5
tracer-ics2019 <tracer@njuics.org>
2026-01-14 12:43:01 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  12:43:01 up  2:32,  3 users,  load average: 0.57, 0.46, 0.33 a0ea8abddd35e9c1ec655bf1195f9ece039e2be

--- navy-apps/libs/libc/src/platform/crt0.c
+++ navy-apps/libs/libc/src/platform/crt0.c
@@ -12,6 +12,8 @@ void _start(int argc, char *argv[], char *envp[]) {
   int ret = main(argc, argv, envp);
   // write(1, &ret, sizeof(ret)); // debug
   write(1, "Exit code: ", 11); // debug
+  write(1, (char *)&ret, sizeof(ret)); // debug
+  write(1, "\n", 1); // debug
   // printf("Program exited with return value %d\n", ret);
   exit(ret);
   assert(0);
