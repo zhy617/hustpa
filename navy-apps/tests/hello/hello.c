bc7a838654d23f8c6838211ddee853e738cb1fba
tracer-ics2019 <tracer@njuics.org>
2026-01-14 19:08:54 +0800

    >  run U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  19:08:54 up  8:58,  3 users,  load average: 0.38, 0.40, 0.29 cef4dd9616fccbba68c668c531c2c13cf787b30a

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,16 +5,10 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (i < 10) {
+  while (1) {
     j ++;
-    write(1, "looping...\n", 11);
-    if (j == 1) {
-      
-      printf("Hello World from Navy-apps for the %dth time!\n", i);
-      
-      // write(1, "Hello World from Navy-apps!\n", 29);
-      i ++;
-
+    if (j == 10000) {
+      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
     }
   }
