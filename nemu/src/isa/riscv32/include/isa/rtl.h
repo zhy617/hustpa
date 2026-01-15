17d692a039dcddb813e47ed40afb4628e0b90afb
tracer-ics2019 <tracer@njuics.org>
2026-01-15 20:07:19 +0800

    >  compile U202210755 mystle Linux PANJU 6.12.57+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.57-1 (2025-11-05) x86_64 GNU/Linux  20:07:19 up 10:47,  3 users,  load average: 0.20, 0.15, 0.16 1f80e3e537c9504e9e0e653eac44b03a599e18ab

--- nemu/src/isa/riscv32/include/isa/rtl.h
+++ nemu/src/isa/riscv32/include/isa/rtl.h
@@ -3,12 +3,12 @@
 
 #include "rtl/rtl.h"
 
-inline void rtl_lr(rtlreg_t* dest, int r, int width) {
+static inline void rtl_lr(rtlreg_t* dest, int r, int width) {
   if (r != 0) { rtl_mv(dest, &reg_l(r)); }
   else { rtl_li(dest, 0); }
 }
 
-static void rtl_sr(int r, const rtlreg_t *src1, int width) {
+static inline void rtl_sr(int r, const rtlreg_t *src1, int width) {
   if (r != 0) { rtl_mv(&reg_l(r), src1); }
 }
 
